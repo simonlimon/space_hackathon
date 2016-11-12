c811015c10a8f0ce2d28b625572e7d515f54e50a
Simon Posada Fishman <simon.posada29@gmail.com>
2016-11-12 02:42:46 -0800

    Fixed sockets

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var screen;
 
-server.listen(process.env.PORT || 5000);
+http.listen(process.env.PORT || 5000);
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('public'))
